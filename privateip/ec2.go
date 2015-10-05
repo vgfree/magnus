cb72fbed482af0838a4741bb70cc7373cb341b9e
Ryan Bourgeois <bluedragonx@gmail.com>
2015-10-04 22:49:12 -0700

    privateip: Add PrivateIP nominator.

---
+++ privateip/ec2.go
@@ -0,0 +1,57 @@
+package privateip
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var (
+	httpClient *http.Client
+	connectTimeout time.Duration = 5 * time.Second
+	responseTimeout time.Duration = 15 * time.Second
+	metadataURL = "http://169.254.169.254/latest/meta-data/"
+)
+
+func init() {
+	dial := func(network, addr string) (net.Conn, error) {
+		return net.DialTimeout(network, addr, connectTimeout)
+	}
+	transport := &http.Transport{Dial: dial, ResponseHeaderTimeout: responseTimeout}
+	httpClient = &http.Client{Transport: transport}
+}
+
+// EC2InstanceID returns the instance ID of the instance running this program.
+func EC2InstanceID() (string, error) {
+	return EC2Metadata("instance-id")
+}
+
+// EC2InterfaceID returns the ID of the primary EC2 interface ID.
+func EC2InterfaceID() (string, error) {
+	macStr, err := EC2Metadata("network/interfaces/macs/")
+	if err != nil {
+		return "", err
+	}
+	macs := strings.Split(macStr, "\n")
+	if len(macs) == 0 {
+		return "", fmt.Errorf("instance has no macs")
+	}
+	return EC2Metadata(fmt.Sprintf("network/interfaces/macs/%s/interface-id", macs[0]))
+}
+
+// EC2Metadata returns the requested EC2 metadata.
+func EC2Metadata(path string) (string, error) {
+	url := metadataURL + path
+	if res, err := httpClient.Get(url); err != nil {
+		return "", err
+	} else if res.StatusCode < 200 || res.StatusCode > 299 {
+		return "", fmt.Errorf("invalid status code %d", res.StatusCode)
+	} else if data, err := ioutil.ReadAll(res.Body); err == nil {
+		return "", err
+	} else {
+		return string(data), nil
+	}
+}
---
+++ privateip/privateip.go
@@ -0,0 +1,73 @@
+package privateip
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"golang.org/x/net/context"
+)
+
+// PrivateIP assigns an available private IP address to the calling EC2 node on
+// nomination.
+type PrivateIP struct {
+	ec2         *ec2.EC2
+	instanceID  string
+	interfaceID string
+	privateIPs  []string
+}
+
+// New creates a new PrivateIP 
+func New(privateIPs []string) (*PrivateIP, error) {
+	if len(privateIPs) == 0 {
+		return nil, errors.New("no private IPs provided")
+	}
+	instanceID, err := EC2InstanceID()
+	if err != nil {
+		return nil, err
+	}
+	interfaceID, err := EC2InterfaceID()
+	if err != nil {
+		return nil, err
+	}
+	return &PrivateIP{
+		ec2:         ec2.New(nil),
+		instanceID:  instanceID,
+		interfaceID: interfaceID,
+		privateIPs:  privateIPs,
+	}, nil
+}
+
+// Nominate chooses and assigns a private IP address to the calling node.
+func (pip *PrivateIP) Nominate(ctx context.Context, name string, size int, leaders map[string]string) (string, error) {
+	assignedIPs := make(map[string]string, len(leaders))
+	for name, ip := range leaders {
+		assignedIPs[ip] = name
+	}
+
+	privateIP := ""
+	for _, ip := range pip.privateIPs {
+		if _, ok := assignedIPs[ip]; !ok {
+			privateIP = ip
+			break
+		}
+	}
+	if privateIP == "" {
+		return "", errors.New("no available private IP addresses")
+	}
+
+	truf := true
+	_, err := pip.ec2.AssignPrivateIpAddresses(&ec2.AssignPrivateIpAddressesInput{
+		NetworkInterfaceId: &pip.interfaceID,
+		PrivateIpAddresses: []*string{&privateIP},
+		AllowReassignment:  &truf,
+	})
+	if err != nil {
+		err = fmt.Errorf("unable to assign IP %s to %s: %s", privateIP, pip.interfaceID, err)
+	}
+	return privateIP, err
+}
+
+// LeaderEvent processes a leader change event. We just discard them.
+func (pip *PrivateIP) LeaderEvent(ctx context.Context, size int, leaders map[string]string) error {
+	return nil
+}
